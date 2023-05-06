ed97cff607917d923b693146b5fcdc08d507d422
Andrey Pyatakha <andrey.pyatakha@gmail.com>
2023-05-07 02:43:36 +0300

    init commit

---
+++ src/main/java/com/itbulls/learnit/spring/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,15 @@
+package com.itbulls.learnit.spring.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.itbulls.learnit.spring.springbootdemo"})
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+
+}
